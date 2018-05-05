2ca8b47e5b3876c1f6d2d3ca2e1baf83e257504b
Julian Hellinga <jchellin@uwaterloo.ca>
2018-05-05 12:11:28 -0400

    Added indicator about colour in the centre of the image

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ var video;
 function afterLoad() {
 	console.log("running after load");
 	// querySelector to get element from HTML video element
-	document.addEventListener('DOMContentLoaded',function() {
-		var v = document.getElementById('videoElement');
-		var c = document.getElementById('c');
-		var context = c.getContext('2d');
-		var cw = Math.floor(c.clientWidth / 100);
-		var ch = Math.floor(c.clientHeight / 100);
-		c.height = cw;
-		c.width = ch;
+	var v = document.getElementById('videoElement');
+	var c = document.getElementById('c');
+	var context = c.getContext('2d');
+	var cw = Math.floor(c.clientWidth);
+	var ch = Math.floor(c.clientHeight);
+	c.height = cw;
+	c.width = ch;
 
-		draw(v,context,cw,ch);
-	});
+	draw(v,context,cw,ch);
 
 	video = document.querySelector("#videoElement");
 
@@ -37,12 +35,11 @@ function videoError(e) {
 } 
 
 function draw(v,c,w,h) {
-	console.log("drawing image on canvas");
+	//console.log("drawing image on canvas");
 	c.drawImage(v,0,0,w,h);
 
-	//data = context.getImageData(w/2,h/2,1,1);
-
-
+	data = c.getImageData(100,100,w/2,h/2);
+	document.getElementById("color_indicator").innerHTML = data.data[0]+" "+data.data[1]+" "+data.data[2];
 
 	setTimeout(draw,20,v,c,w,h);
 }
